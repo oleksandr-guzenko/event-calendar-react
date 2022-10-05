6df6d11cf37e2406d93eb5cbcdf5cb9335001248
dragonwarrior980706 <dragon.warrior980706@gmail.com>
2022-10-05 12:03:28 +0900

    add empty actions and reducers

---
+++ client/src/components/actions/types.js
@@ -0,0 +1,7 @@
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const GET_ACCOUNTS = 'GET_ACCOUNTS';
+export const GET_ACCOUNT = 'GET_ACCOUNT';
+export const ADD_ACCOUNT = 'ADD_ACCOUNT';
+export const UPDATE_ACCOUNT = 'UPDATE_ACCOUNT';
+export const DISABLE_ACCOUNT = 'DISABLE_ACCOUNT';
